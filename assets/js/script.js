a4f0b85c12f9b109caf7cb97e20756507838e235
yinzhedy <yinzhe.dy@gmail.com>
2021-09-18 00:25:01 -0400

    added in current day display, got moment in js for that working.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,27 @@ var eventNameInput = document.querySelector(".event-name-input");
 var eventDescriptionInput = document.querySelector(".event-description-input");
 var timeLabel = document.querySelector("#time-label");
 var saveButton = document.querySelector(".save-button");
-var currentDay = document.querySelector("#current-day");
+var currentDay = document.getElementById("current-day");
 
 console.log(eventNameInput);
 console.log(eventDescriptionInput);
 console.log(timeLabel, "");
 
 saveButton.addEventListener("click", function saveInfo() {
-    saveButton.addEventListener("click", function () {
-        console.log(this);
-        var description = eventDescriptionInput.value;
-        var name = eventNameInput.value;
-        // var time = timeLabel.text;
-        localStorage.setItem('name', (name));
-        localStorage.setItem('description', (description));
-})});
+  saveButton.addEventListener("click", function () {
+    console.log(this);
+    var description = eventDescriptionInput.value;
+    var name = eventNameInput.value;
+    // var time = timeLabel.text;
+    localStorage.setItem("name", name);
+    localStorage.setItem("description", description);
+  });
+});
 
-eventNameInput.value = localStorage.getItem('name');
-eventDescriptionInput.value = localStorage.getItem('description');
+eventNameInput.value = localStorage.getItem("name");
+eventDescriptionInput.value = localStorage.getItem("description");
 
-function displayDay() {
-    currentDay.textContent = moment().toDate()
-}
+var today = moment();
+currentDay.textContent = today.format("MMM Do, YYYY");
+console.log(currentDay.textContent)
+currentDay.textContent
